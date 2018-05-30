2896192fc12d82d31bacec95fc21599ba10f0fea
aghebert <30908336+aghebert@users.noreply.github.com>
2018-05-30 19:39:18 -0400

    MVC3-MVC5

---
+++ MVC Labs/MVC_Lab5 copy/MVC_Lab5/Models/CarFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVC_Lab5.Models
+{
+    public class CarFactory : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public CarFactory()
+        {
+            Database.SetInitializer(new CarInitializer());
+        }
+    }
+
+    public class CarInitializer : DropCreateDatabaseIfModelChanges<CarFactory>
+    {
+        protected override void Seed(CarFactory context)
+        {
+            {
+                context.Cars.Add(new Car() { Model = "Rabbit", MaxSpeed = 300 });
+                context.Cars.Add(new Car() { Model = "Turtle", MaxSpeed = 150 });
+
+            }
+        }
+    }
+
+}
