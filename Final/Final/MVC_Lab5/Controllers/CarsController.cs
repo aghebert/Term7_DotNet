d7e7729f21e22cbe09d01a5742fc516de7c486e4
aghebert <30908336+aghebert@users.noreply.github.com>
2018-05-31 18:09:58 -0400

    Working on Final

---
+++ Final/Final/MVC_Lab5/Controllers/CarsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Final.Models;
+using Final.ViewModels.Home;
+
+namespace Final.Controllers
+{
+    public class CarsController : Controller
+    {
+
+        private CarFactory Db = new CarFactory();
+
+        // GET: Cars
+        public ActionResult CarsList()
+        {
+            var factory = new CarFactory();
+            var viewModel = new CarsListViewModel(factory.Cars);
+
+            return View(viewModel);
+        }
+
+        public ActionResult ListofCars(string searchCriteria)
+        {
+            /*
+            var factory = new CarFactory();
+
+            var cars = new CarFactory().Cars.ToList();
+
+            return View(cars);
+            */
+
+            var factory = new CarFactory();
+
+            IQueryable<Car> cars = factory.Cars.OrderBy(p => p.Model);
+
+            if (searchCriteria != null)
+            {
+                cars = cars.Where(p => p.Model.Contains(searchCriteria));
+            }
+
+            var carlist = cars.Take(1).ToList();
+            return View(carlist);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var factory = new CarFactory();
+
+            Car found = factory.Cars.Where(p => p.Car_ID == id).FirstOrDefault();
+
+            return View(found);
+        }
+    }
+}
